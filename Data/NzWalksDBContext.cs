edee3c0bb42eb089b78fe15aae4e3c05cda1c039
electroo0 <90555965+amitjimiwal@users.noreply.github.com>
2024-09-02 15:34:12 +0530

    initital migrations code

---
+++ Data/NzWalksDBContext.cs
@@ -0,0 +1,17 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using apiNZWalks.Models.Domain;
+namespace apiNZWalks.Data
+{
+	public class NzWalksDBContext : DbContext
+	{
+		public NzWalksDBContext(DbContextOptions dboptions):base(dboptions)
+		{
+				
+		}
+		public DbSet<Difficulty> Difficulties { get; set; }
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+    }
+}
+
