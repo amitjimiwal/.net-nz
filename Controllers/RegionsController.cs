d3baf83fd61f244b025b37cc509382895facab13
electroo0 <90555965+amitjimiwal@users.noreply.github.com>
2024-09-03 11:44:43 +0530

    dto

--- Controllers/RegionsController.cs
+++ Controllers/RegionsController.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using apiNZWalks.Models.Domain;
+using apiNZWalks.Data;
+using apiNZWalks.Models.DTO;
+using Microsoft.Identity.Client;
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace apiNZWalks.Controllers
@@ -12,29 +15,61 @@ namespace apiNZWalks.Controllers
     [ApiController]
     public class RegionsController : Controller
     {
+        private readonly NzWalksDBContext _context;
+        public RegionsController(NzWalksDBContext context)
+        {   
+            this._context = context;
+        }
         // GET: /<controller>/
         [HttpGet]
         public IActionResult GetAll()
         {
-            var regions = new List<Region>
-            {
-                new Region()
-                {
-                    Id=Guid.NewGuid(),
-                    Name="Auckland Region",
-                    Code=" ",
-                    RegionImageUrl="https://dcblog.b-cdn.net/wp-content/uploads/2021/02/Full-form-of-URL-1.jpg"
-                },
-                new Region()
-                {
-                    Id=Guid.NewGuid(),
-                    Name="Auckland Region",
-                    Code=" ",
-                    RegionImageUrl="https://dcblog.b-cdn.net/wp-content/uploads/2021/02/Full-form-of-URL-1.jpg"
-                }
-            };
+            //get data from the database
+            var regions = _context.Regions.ToList();
 
-            return Ok(regions);
+            //return the dto instead of the entity from database - good practice
+            List<RegionsDTO> regionsDTO = new List<RegionsDTO>();
+            foreach (var region in regions)
+            {
+                regionsDTO.Add(new RegionsDTO
+                {
+                    Id = region.Id,
+                    Code = region.Code,
+                    Name = region.Name,
+                    RegionImageUrl = region.RegionImageUrl
+                });
+            }
+            return Ok(regionsDTO);
+        }
+
+        //get region by id 
+        //make sure the {variable} name matches the parameter name
+        [HttpGet]
+        [Route("{id}")]
+        public IActionResult GetByRegionID([FromRoute] Guid id){
+            var region = _context.Regions.Find(id);
+
+            //404 if not found
+            if(region == null){
+                return NotFound();
+            }
+            
+
+            //convert the output into DTO
+            RegionsDTO  result=new RegionsDTO(){
+                Id=region.Id,
+                Code=region.Code,
+                Name=region.Name,
+                RegionImageUrl=region.RegionImageUrl
+            };
+            
+            return Ok(result);
+        }
+
+        [HttpPost]
+        [Route("add")]
+        public IActionResult createRegion([FromBody] RegionsDTO dtt){
+            return Ok("Working");
         }
     }
 }
---
+++ Models/DTO/RegionsDTO.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace apiNZWalks.Models.DTO{
+     public class RegionsDTO
+     {
+        public Guid Id { get; set; }
+
+        public string Code { get; set; }
+
+        public string Name { get; set; }
+
+        public string? RegionImageUrl { get; set; }
+     }
+}
+
